85c03c2fb95cf6e2b98dd28e6624c93d35dc7f57
brodeau <brodeau@gmail.com>
2023-04-26 19:26:18 +0200

    LB

--- tools/generate_idealized_seeding.py
+++ tools/generate_idealized_seeding.py
@@ -25,7 +25,7 @@ lRandomize = True
 
 seeding_type='debug' ; # By default, ha
 
-ldo_coastal_clean = False
+ldo_coastal_clean = True
 MinDistFromLand  = 100 ; # how far from the nearest coast should our buoys be? [km]
 fdist2coast_nc = 'dist2coast/dist2coast_4deg_North.nc'
 
@@ -154,31 +154,31 @@ if __name__ == '__main__':
             #rd_ss =  6.2 ; # to test !!!! => 10.05
             rd_ss =  6. ; # to test !!!! => fine...
             zAmpRand = 0.05 ; # degrees
-            ldo_coastal_clean=True; distMax=300 ; distMin = 100 ; # how far from the nearest coast should our buoys be? [km]            
+            #ldo_coastal_clean=True; distMax=300 ; distMin = 100 ; # how far from the nearest coast should our buoys be? [km]            
         elif icrsn==20:
             rd_ss = 14.6 ; # real shit! 
             zAmpRand = 0.05 ; # degrees
-            ldo_coastal_clean=True; distMax=300 ; distMin = 100 ; # how far from the nearest coast should our buoys be? [km]            
+            #ldo_coastal_clean=True; distMax=300 ; distMin = 100 ; # how far from the nearest coast should our buoys be? [km]            
         elif icrsn==40:
             rd_ss = 34.5 ; # real shit!
-            zAmpRand = 0.1 ; # degrees
-            ldo_coastal_clean=True; distMax=300 ; distMin = 100 ; # how far from the nearest coast should our buoys be? [km]            
+            zAmpRand = 0.15 ; # degrees
+            #ldo_coastal_clean=True; distMax=300 ; distMin = 100 ; # how far from the nearest coast should our buoys be? [km]            
         elif icrsn==80:
             rd_ss = 74.75 ; # real shit!
-            zAmpRand = 0.2 ; # degrees
-            ldo_coastal_clean=True; distMax=300 ; distMin = 100 ; # how far from the nearest coast should our buoys be? [km]
+            zAmpRand = 0.15 ; # degrees
+            #ldo_coastal_clean=True; distMax=300 ; distMin = 100 ; # how far from the nearest coast should our buoys be? [km]
         elif icrsn==160:
             rd_ss = 156. ; # real shit!
-            zAmpRand = 0.25 ; # degrees
-            ldo_coastal_clean=True; distMax=300 ; distMin = 100 ; # how far from the nearest coast should our buoys be? [km]
+            zAmpRand = 0.15 ; # degrees
+            #ldo_coastal_clean=True; distMax=300 ; distMin = 100 ; # how far from the nearest coast should our buoys be? [km]
         elif icrsn==320:
             rd_ss = 315.6 ; # real shit!
-            zAmpRand = 0.5 ; # degrees
-            ldo_coastal_clean=True; distMax=300 ; distMin = 100 ; # how far from the nearest coast should our buoys be? [km]
+            zAmpRand = 0.15 ; # degrees
+            #ldo_coastal_clean=True; distMax=200 ; distMin = 200 ; # how far from the nearest coast should our buoys be? [km]
         elif icrsn==640:
             rd_ss = 636. ; # real shit!
-            zAmpRand = 0.5 ; # degrees
-            ldo_coastal_clean=True; distMax=300 ; distMin = 100 ; # how far from the nearest coast should our buoys be? [km]
+            zAmpRand = 0.15 ; # degrees
+            #ldo_coastal_clean=True; distMax=300 ; distMin = 100 ; # how far from the nearest coast should our buoys be? [km]
         else:
             print('ERROR: we do not know what `rd_ss` to pick for `icrsn` =',icrsn)
             exit(0)
